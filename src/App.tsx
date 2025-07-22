f3f6962a5b60a3f7b80049ef112e59fe620a75ee
Muhamad Yopan Ramadhan <yopanramadhan8@gmail.com>
2025-07-22 21:46:35 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-green-500">hello word</h1>
+
+      <Button>tes button shadcn</Button>
+    </div>
+  );
+}
+
+export default App;
